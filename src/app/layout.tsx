87f5ae4f7dce0c3db04c4e05eb08cc202b0ea6c4
Badr Damou <badrdamou01@gmail.com>
2025-10-30 17:55:49 +0100

    my first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+import Footer from '@/components/Footer/Footer'
+import Navbar from '@/components/Navbar/Navbar'
+import ThemeMenu from '@/components/Theme/ThemeMenu'
+import { Fira_Code } from 'next/font/google'
+
+const firaCode = Fira_Code({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
+
+const title = 'Badr Damou | Full-Stack Web Developer in Casablanca'
+
+const description =
+  "Skilled full-stack web developer in Chicago. I build responsive, user-friendly websites with React, NextJS, and NodeJS. Let's bring your vision to life. Hire me today!"
+
+export const metadata: Metadata = {
+  title,
+  description,
+  category: 'technology',
+  // Removed metadataBase + alternates since no backend or env vars are needed
+  openGraph: {
+    title,
+    description,
+    siteName: 'Badr Damou Portfolio',
+    type: 'website',
+  },
+  twitter: {
+    title,
+    description,
+    card: 'summary_large_image',
+    creator: '@Basit_Miyanji',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" data-theme="dark">
+      <body className={`${firaCode.className}`}>
+        <header>
+          <Navbar />
+        </header>
+        {children}
+        <ThemeMenu />
+        <Footer />
+      </body>
+    </html>
+  )
+}
